02d9b21e6bf707eb27ee56c1243ed9709c0756ac
Martin Brandenburg <martin@dunkle.(none)>
2009-03-19 22:18:13 -0400

    Keyboard shift support.

--- include/keyboard.h
+++ include/keyboard.h
@@ -3,6 +3,14 @@
 
 #include <isr.h>
 
+typedef struct {
+	unsigned int alt;
+	unsigned int ctrl;
+	unsigned int shift_l;
+	unsigned int shift_r;
+	unsigned int capslock;
+} keyboard_status_t;
+
 unsigned int keyboard_install(void);
 char keyboard_getchar();
 
