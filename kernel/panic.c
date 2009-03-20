f16cbc12b3b41d09ec718f7ee014d6e7f97aa1d4
Martin Brandenburg <martin@dunkle.(none)>
2009-03-19 21:17:09 -0400

    Stack dumps on panic.

--- kernel/panic.c
+++ kernel/panic.c
@@ -7,6 +7,18 @@ void panic( char * text )
 	/* init_video(); */
 	puts("\nPanic: ");
 	puts(text);
+	stack_dump();
 	asm("cli");
 	asm("hlt");
+}
+
+void panic_dump_hex(unsigned int *stack)
+{
+	unsigned int orig_stack = (unsigned int) stack;
+
+	printk("\nStack Contents\n");
+	while (stack < ((orig_stack+0x1000) & ~(0x1000-1))) {
+		printk("0x%x: 0x%x\n", stack, *stack);
+		stack++;
+	}
 }
