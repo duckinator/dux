b63cdafcae46ced494522d42a35fe73aab790ede
Martin Brandenburg <martin@computer.(none)>
2009-04-26 01:56:40 -0400

    Modular paging.

--- mm/memory.c
+++ mm/memory.c
@@ -112,48 +112,59 @@ void free_frame(PageTableEntry *page)
 	page->address = 0x0;
 }
 
-PageDirEntry *page_directory;
+PageDirEntry *cur_page_directory;
 
-void init_mm()
+/* clear_page_directory(page_directory)
+ * Clears the page directory given.
+ */
+void clear_page_directory(PageDirEntry* page_directory)
 {
 	unsigned int i;
-	unsigned int address;
-	PageTableEntry* page_table;
-	
-	// Memory is assumed to be 16 MB.
-	frames = (unsigned int*) kmalloc_int(END_MEMORY/0x1000, NULL);
-	memset(frames, 0, END_MEMORY/0x1000/8);
-
-	// Set page directory
-	page_directory = (PageDirEntry*) kmalloc_int(sizeof(PageDirEntry)*1024, MALLOC_ALIGN);
-
-	// Clear the entire page directory.
 	for (i = 0; i < 1024; i++) {
 		page_directory[i].present = 0;
 		page_directory[i].readwrite = 1;
 		page_directory[i].user = 0;
 	}
+}
 
-	// Create a page table.
-	page_table = (PageTableEntry*) kmalloc_int(sizeof(PageTableEntry)*1024, MALLOC_ALIGN);
-
-	// Identity map the page table (first 4MB of memory).
-	address = 0;
+/* idmap_page_table(page_table, address)
+ * Identity maps the page table given to address.
+ */
+void idmap_page_table(PageTableEntry* page_table, unsigned int address)
+{
+	unsigned int i;
 	for (i = 0; i < 1024; i++, address += 0x1000) {
 		page_table[i].address = address >> 12;
 		page_table[i].present = 1;
 		page_table[i].readwrite = 1;
 		page_table[i].user = 0;
 	}
+}
+
+void init_mm()
+{
+	PageTableEntry* page_table;
+	
+	// Memory is assumed to be 16 MB.
+	frames = (unsigned int*) kmalloc_int(END_MEMORY/0x1000, NULL);
+	memset(frames, 0, END_MEMORY/0x1000/8);
+
+	// Create page directory
+	cur_page_directory = (PageDirEntry*) kmalloc_int(sizeof(PageDirEntry)*1024, MALLOC_ALIGN);
+	clear_page_directory(cur_page_directory);
+
+	// Create a page table.
+	page_table = (PageTableEntry*) kmalloc_int(sizeof(PageTableEntry)*1024, MALLOC_ALIGN);
+	idmap_page_table(page_table, 0);
 
 	// Add that page table to the page directory.
-	page_directory[0].address = (unsigned int) page_table >> 12;
-	page_directory[0].present = 1;
-	page_directory[0].readwrite = 1;
-	page_directory[0].user = 0;
+	cur_page_directory[0].address = (unsigned int) page_table >> 12;
+	cur_page_directory[0].present = 1;
+	cur_page_directory[0].readwrite = 1;
+	cur_page_directory[0].user = 0;
 
 	// Enable paging.
-	asm volatile("mov %0, %%cr3":: "b"(page_directory));
+	asm volatile("mov %0, %%cr3":: "b"(cur_page_directory));
 	unsigned int cr0;
 	asm volatile("mov %%cr0, %0": "=b"(cr0));
 	cr0 |= 0x80000000;
