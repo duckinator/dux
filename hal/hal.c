66b2852d80d801ea4c41f4c89e4345a13f450855
Ellen Marie Dash <me@duckie.co>
2009-07-16 00:33:46 -0400

    Weeee...... it should work on real hardware now! :D

--- hal/hal.c
+++ hal/hal.c
@@ -1,8 +1,16 @@
+#include <buildnum.h>
 #include <hal.h>
 
 void HalInit(void (*receiver)(uint32_t msg))
 {
 	char c;
+
+	// Start the console
+	console_init();
+	screen_setattr(0x80, 0x80);
+	screen_clear();
+
+	printk("Dux OS Build %d %d\n", BUILDNUM);
 
 	// Enable interrupts
 	HalIsrInstall();
