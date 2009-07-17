1d277b44f0b88f23ddd202b134635b77a36b99fc
Daniel Danopia <me.github@danopia.net>
2009-07-17 02:38:59 -0400

    If this doesn't add the change, I'll kill myself.

--- include/dux/drivers/core/console.h
+++ include/dux/drivers/core/console.h
@@ -2,6 +2,8 @@ void console_writeb(char c);
 void console_write(char *buf, unsigned int count);
 char console_readb();
 
+void console_tab_start(char start);
+
 void console_init();
 
 void console_clear();
