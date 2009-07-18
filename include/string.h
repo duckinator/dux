600bc59f8686f79c3b73bfd7d99a502a0f10d025
Scott Olson <scott@scott-olson.org>
2009-07-18 16:16:03 -0600

    Fixed up string.h

--- include/string.h
+++ include/string.h
@@ -1,18 +1,23 @@
-#ifndef __size_t_defined
-typedef signed int size_t;
-#define __size_t_defined
-#endif
+#include <stddef.h>
 
-void *memcpy(void *dest, void *src, unsigned int count);
-void *memset(void *dest, unsigned char val, unsigned int count);
-unsigned short *memsetw(unsigned short *dest, unsigned short val, unsigned int count);
+#ifndef __STRING_H
+#define __STRING_H
+
+#include <stddef.h>
+
 int strlen(const char *str);
 
 char *strcpy(char *s, char *ct);
 char *strncpy(char *s, char *ct, int n);
+
 char *strcat(char *s, char *ct);
 char *strncat(char *s, char *ct, int n);
+
 int strcmp(char *cs, char *ct);
 int strncmp(char *cs, char *ct, int n);
+
 char *strchr(char *cs, char c);
-//char *strrchr(char *cs, char c);
+char *strrchr(char *cs, char c);
+
+#endif /* end of include guard: __STRING_H */
+
