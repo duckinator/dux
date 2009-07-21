008d6717426a90d1ef105413f0082e0402b8242e
Martin Brandenburg <brandenburgmk@gmail.com>
2009-07-21 15:01:17 -0400

    Shutdown uses arch.

--- krnl/misc/shutdown.c
+++ krnl/misc/shutdown.c
@@ -2,12 +2,15 @@
 #include <dux/drivers/core/screen.h>
 #include <dux/shutdown.h>
 
+#include <arch/display.h>
+#include <arch/shutdown.h>
+
 void DuxShutdown(void)
 {
-	console_init();
-	screen_setattr(0x1f, 0x1f);
-	screen_clear();
-	printk("Shutting down...\n");
-	printk("It is now safe to turn off your computer.\n");
-	while (1);
+	ArchDisplayInit();
+	ArchDisplayClear();
+	ArchDisplayString("Shutting down...\n");
+	ArchDisplayString("It is now safe to turn off your computer.\n");
+
+	ArchShutdown();
 }
