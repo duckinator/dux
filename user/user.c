d7f1513c77e51a1abcecc7b2d3dfae9397d30da9
Martin Brandenburg <brandenburgmk@gmail.com>
2009-07-24 20:41:26 -0400

    Fix userland.

--- user/user.c
+++ user/user.c
@@ -1,6 +1,9 @@
 void _start(void)
 {
-	main();
+	char **argv = {
+		"user"
+	};
+	main(1, argv);
 	while (1)
 		;
 }
@@ -8,14 +11,16 @@ void _start(void)
 int main(int argc, char const* argv[])
 {
 	__asm__ __volatile__ (" \
-			mov $1, %eax; \
-			mov $1024, %ebx; \
+			mov $1, %%eax; \
+			mov $1024, %%ebx; \
 			int $0x80; \
-			");
+			" : : : "eax");
 
 	char *a = "a";
-	__asm__ __volatile__ ("mov $0, %eax");
-	__asm__ __volatile__ ("mov %0, %%eax" : : "m"(&a[0]) : "eax");
-	__asm__ __volatile__ ("int $0x80");
+	__asm__ __volatile__ (" \
+			mov $0, %%eax; \
+			mov %0, %%ebx; \
+			int $0x80; \
+			" : : "m" (a) : "eax", "ebx");
 	return 0;
 }
