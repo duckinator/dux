6a4584b25fc0c1b279df863e27733aa0a5859021
Ellen Marie Dash <me@duckie.co>
2009-07-26 01:24:56 -0400

    krnllib.lib works! We now have puts() puthex() and putdec() in userland

--- src/user/user.c
+++ src/user/user.c
@@ -10,6 +10,7 @@ void _start(void)
 
 int main(int argc, char const* argv[])
 {
+	/*
 	__asm__ __volatile__ (" \
 			mov $1, %%eax; \
 			mov $1024, %%ebx; \
@@ -22,5 +23,11 @@ int main(int argc, char const* argv[])
 			mov %0, %%ebx; \
 			int $0x80; \
 			" : : "m" (a) : "eax", "ebx");
+	*/
+	puthex(1024);
+	puts("\n");
+	putdec(1024);
+	puts("\n");
+	puts("ohi");
 	return 0;
 }
