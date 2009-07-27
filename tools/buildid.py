8aadd9befdb6710dd98416fbe4be6a9db5d4025a
Martin Brandenburg <martin@desk.(none)>
2009-07-27 19:27:56 -0400

    Add buildid.py.

---
+++ tools/buildid.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+import subprocess
+
+p = subprocess.Popen('git show | head -n1 | cut -b8-', shell=True, stdout=subprocess.PIPE)
+rev = p.stdout.readline().replace('\n', '')
+p.stdout.close()
+
+print('#define SCM_REV "{0}"'.format(rev))
