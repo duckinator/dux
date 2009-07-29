12f5d8df49ec752fa8345831fb8112b124e6c422
Martin Brandenburg <brandenburgmk@gmail.com>
2009-07-28 21:00:52 -0400

    Fixed IPL code.

--- include/krnl/stop.h
+++ include/krnl/stop.h
@@ -7,7 +7,7 @@
 #define STOP_UNKNOWN 0x1
 #define STOP_BAD_MULTIBOOT_SIGNATURE 0x2
 #define STOP_NO_USERLAND 0x3
-#define STOP_IPL_NOT_GREATER_OR_EQUAL 0x4
+#define STOP_IPL_NOT_LESS_OR_EQUAL 0x4
 
 void KrnlStop(uint32_t error, uint32_t arg1, uint32_t arg2, uint32_t arg3, uint32_t arg4);
 #define KrnlEasyStop(error) KrnlStop(error, 0, 0, 0, 0);
