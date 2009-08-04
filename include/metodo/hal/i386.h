3178ad33be293f27a786cd0c449605cf9de441a3
Martin Brandenburg <brandenburgmk@gmail.com>
2009-08-04 13:48:07 -0400

    Add support to edit and load the IDT.

--- include/metodo/hal/i386.h
+++ include/metodo/hal/i386.h
@@ -2,6 +2,31 @@
 
 #define I386_TBY8VD0E
 
+/* IDT */
+typedef struct IDTD {
+	uint16_t size;
+	uint32_t offset;
+} IDTD, *PIDTD;
+
+typedef struct IDTG {
+	uint16_t offset_1;
+	uint16_t selector;
+	uint8_t zero;
+	uint8_t type;
+	uint16_t offset_2;
+} IDTG, *PIDTG;
+
+#define IDT_TASK 0x5
+#define IDT_INTR16 0x6
+#define IDT_INTR32 0x7
+#define IDT_TRAP16 0xe
+#define IDT_TRAP32 0xf
+
+void HalInitIDT(void);
+void HalLoadIDT(void);
+void HalSetIDTGate(OUT PIDTG idtg, IN uint32_t offset, IN uint16_t selector, IN uint8_t priv, IN uint8_t sys, IN uint8_t gatetype);
+
+/* Ports */
 inline void HalOutPort(uint16_t port, uint8_t val);
 inline uint8_t HalInPort(uint16_t port);
 
---
+++ src/metodo/lib/zeromemory.c
@@ -0,0 +1,14 @@
+#include <metodo/metodo.h>
+
+void *ZeroMemory(IN void *ptr, IN uint32_t len)
+{
+	int i;
+	uint8_t *mem = ptr;
+
+	for (i = 0; i < len; i++) 
+	{
+		mem[i] = 0;
+	}
+
+	return mem;
+}
