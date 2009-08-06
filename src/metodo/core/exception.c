eb966731b22e84684fb39f362b849174f8d0e423
Martin Brandenburg <brandenburgmk@gmail.com>
2009-08-05 23:47:02 -0400

    Cleaning up old code.

--- src/metodo/core/exception.c
+++ src/metodo/core/exception.c
@@ -3,11 +3,8 @@
 void CoException(void)
 {
 	char *str = "The system crashed. I don't know why.";
-	uint16_t *vidmem = (uint16_t*)0xb80a0;
-	int i = 0;
 
-	while (i < 80)
-		vidmem[i] = 0x1f << 8 | str[i++];
+	HalDisplayString(str);
 
 	while (1)
 		;
