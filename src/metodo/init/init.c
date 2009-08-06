70cc0991e543ccb3dd5f224a0fd64e8859d32a5b
Martin Brandenburg <brandenburgmk@gmail.com>
2009-08-05 23:47:02 -0400

    Cleaning up old code.

--- src/metodo/init/init.c
+++ src/metodo/init/init.c
@@ -4,14 +4,10 @@
 
 void InInitKernel(void)
 {
-	char *str = "Metodo " __DATE__ " " __TIME__ " " ARCH " " SCM_REV;
-	uint16_t *vidmem = (uint16_t*)0xb8000;
-	int i = 0;
+	char *str = "Metodo " __DATE__ " " __TIME__ " " ARCH " " SCM_REV "\n";
 
 	HalInit();
 
-/*	while (i < 80)
-		vidmem[i++] = 0x1f << 8 | *str++;*/
 	HalDisplayString(str);
 
 	asm volatile ("int $0");
