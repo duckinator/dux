4dcb8032060509fb0e429af44e70b9f2181bf178
Ellen Marie Dash <me@duckie.co>
2009-11-01 22:50:57 -0500

    Cleaning up include/ (removed include/arch and include/dux, not used by anything but out-of-date stuff in user/ which is not compiled in)

--- include/arch/i386/arch/shutdown.h
+++
@@ -1,9 +0,0 @@
-#ifndef SHUTDOWN_I6F54URD
-
-#define SHUTDOWN_I6F54URD
-
-void ArchBreak(void);
-void ArchReboot(void);
-void ArchShutdown(void);
-
-#endif /* end of include guard: SHUTDOWN_I6F54URD */
