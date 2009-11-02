09dd3433ddcd70f60b2b3d265b0e25e6760d4dc0
Liam Dunn <liam.dunn144@gmail.com>
2009-11-02 15:25:31 +1100

    Working mm, I think *function names are not in line with all other HAL function names, but meh.

--- include/metodo/misc.h
+++ include/metodo/misc.h
@@ -3,4 +3,8 @@
 
 void panic(char *text);
 
+void *memcpy(void *dest, void *src, unsigned int count);
+extern void *memset(void *dest, unsigned char val, unsigned int count);
+unsigned short *memsetw(unsigned short *dest, unsigned short val, unsigned int count);
+int strlen(const char *str);
 #endif
