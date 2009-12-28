0c5a274494b1ef99eb7959772a4eb877f65838ad
Ellen Marie Dash <me@duckie.co>
2009-12-27 20:40:38 -0500

    Cleaned up RamdiskTest() so it only prints anything if userland is passed

--- src/metodo/init/tests.c
+++ src/metodo/init/tests.c
@@ -54,37 +54,34 @@ void MMTest()
 
 void RamdiskTest(void *ramdisk)
 {
-	printf("Beginning ramdisk/fs_root test:\n");
-	int i;
-	fs_node_t *fs_root;
 	if (ramdisk != NULL) {
+		printf("Beginning ramdisk/fs_root test:\n");
+		int i;
+		fs_node_t *fs_root;
 		fs_root = initialise_initrd((uint32_t)ramdisk);
 		printf ("\tfs_root = 0x%x\n\n", fs_root);
 
-  	printf("\tListing contents of initrd:\n");
-	  i = 0;
-  	struct dirent *node = 0;
-  	while ( (node = readdir_fs(fs_root, i)) != 0)
-  	{
-      if ( strlen(node->name) == 0) {
-        printf("\t\tWhy did I find an empty filename?\n");
-      }
-      else {
-    		printf("\t\tFound file: %s\n", node->name);
-    		fs_node_t *fsnode = finddir_fs(fs_root, node->name);
+		printf("\tListing contents of initrd:\n");
+		i = 0;
+		struct dirent *node = 0;
+		while ( (node = readdir_fs(fs_root, i)) != 0) {
+			if ( strlen(node->name) == 0) {
+				printf("\t\tWhy did I find an empty filename?\n");
+			} else {
+				printf("\t\tFound file: %s\n", node->name);
+				fs_node_t *fsnode = finddir_fs(fs_root, node->name);
 
-    		if((fsnode->flags&0x7) == FS_DIRECTORY)
-    			printf("\t\t\tIt's a directory!\n");
-  		  else
-    		{
-    			printf("\t\tContents: ");
-    			char *buf;
-    			uint32_t size = read_fs(fsnode, 0, 512, buf);
-    			printf("%s\n", buf);
-    		}
-  		}
-  		i++;
-  	}
-  }
-  printf("Finished ramdisk/fs_root test\n\n");
+    		if((fsnode->flags&0x7) == FS_DIRECTORY) {
+					printf("\t\t\tIt's a directory!\n");
+				} else {
+					printf("\t\tContents: ");
+					char *buf;
+					uint32_t size = read_fs(fsnode, 0, 512, buf);
+					printf("%s\n", buf);
+				}
+			}
+			i++;
+		}
+		printf("Finished ramdisk/fs_root test\n\n");
+	}
 }
