d1dcfdeeacfdfcdd2368bf4489ea7637520b0fa9
Ellen Marie Dash <me@duckie.co>
2010-02-05 22:27:21 -0500

    Fixed tabbing....again

--- src/metodo/colpa/debug.c
+++ src/metodo/colpa/debug.c
@@ -2,43 +2,43 @@
 
 void dprint(const char *msg, debug_type type)
 {
-  if (DEBUG_MODE == NONE)
-    return;
+	if (DEBUG_MODE == NONE)
+		return;
 
-  if (DEBUG_MODE == ALL || type == DEBUG_MODE)
-    printf(msg);
+	if (DEBUG_MODE == ALL || type == DEBUG_MODE)
+		printf(msg);
 }
 
 void set_debug_mode(debug_type type)
 {
-  DEBUG_MODE = type;
+	DEBUG_MODE = type;
 }
 
 debug_type get_debug_mode(void)
 {
-  return DEBUG_MODE;
+	return DEBUG_MODE;
 }
 
 
 void KernDebug()
 {
-  HalKeyInfo keyinfo;
-  printf("Initiating kernel debugger...\n>> ");
-  while(1) {
-    if ( HalKeyboardHasInput() ) {
-      keyinfo = HalKeyboardReadLetter();
-      if ( keyinfo.action == 0 ) {
-	switch ( keyinfo.scancode ) {
-	  // Enter
-	  case 0x9c:
-	    printf("\n>> ");
-	    break;
-	  // Otherwise
-	  default:
-	    printf("%c", keyinfo.key);
-            break;
+	HalKeyInfo keyinfo;
+	printf("Initiating kernel debugger...\n>> ");
+	while(1) {
+		if ( HalKeyboardHasInput() ) {
+			keyinfo = HalKeyboardReadLetter();
+			if ( keyinfo.action == 0 ) {
+				switch ( keyinfo.scancode ) {
+				// Enter
+				case 0x9c:
+					printf("\n>> ");
+					break;
+				// Otherwise
+				default:
+					printf("%c", keyinfo.key);
+					break;
+				}
+			}
+		}
 	}
-      }
-    }
-  }
 }
