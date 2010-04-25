c7709534eb98a0407e97079416640a6c94249313
Nick Markwell <nick@duckinator.net>
2010-04-24 23:38:07 -0400

    Minor fix to UART driver

--- src/metodo/hal/i386/serial.c
+++ src/metodo/hal/i386/serial.c
@@ -25,7 +25,7 @@ static void UartSend_int(char c)
 int UartInit()
 {
 	HalOutPort(UART_BASE_REGISTER + LineControl, DLAB);
-	HalOutPortWord(UART_BASE_REGISTER, UART_BAUDRATE_DIVISOR);
+	HalOutPort(UART_BASE_REGISTER, UART_BAUDRATE_DIVISOR);
 	HalOutPort(UART_BASE_REGISTER + LineControl, UART_LCR);
 	HalOutPort(UART_BASE_REGISTER + ModemControl, 0);
 
