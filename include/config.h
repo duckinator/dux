2fb4b460ae647be2d3df9ef46533573912bcbf0d
Ellen Marie Dash <me@duckie.co>
2010-06-04 01:15:58 -0400

    Added comments to include/config.h, added IA32_SYSCALL_INTERRUPT

--- include/config.h
+++ include/config.h
@@ -1,6 +1,15 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-//#define PRINTF_USE_MM // Uncomment for printf() and friends to use memory management
+/* The following line, if defined, has printf() and friends use memory management.
+ * If commented, they will not use memory management at all
+ */
+//#define PRINTF_USE_MM
+
+/* Interrupt for syscalls:
+ *   default is 0x20
+ *   used to use 0x80 (got the idea from Linux)
+ */
+#define IA32_SYSCALL_INTERRUPT 0x20
 
 #endif
