b59f6cd58a91ad39294ce6d997b3a98600493f9c
Ellen Marie Dash <me@duckie.co>
2010-06-04 01:09:01 -0400

    Holy crap, I practically gutted the entire syscall system....

--- src/metodo/hal/i386/syscall.c
+++ src/metodo/hal/i386/syscall.c
@@ -35,29 +35,21 @@ void *malloc(unsigned int size, unsigned int flags)
 	return kmalloc_int(size, flags);
 }
 
-DEFN_SYSCALL1(ULMonitorWriteChar, 0, const char);
-DEFN_SYSCALL1(ULMonitorWriteHex, 1, const uint32_t); // hex
-DEFN_SYSCALL1(ULMonitorWriteDec, 2, const uint32_t); // dec
-DEFN_SYSCALL0(ULShutdown,        3);
-DEFN_SYSCALL0(ULReboot,          4);
-DEFN_SYSCALL0(HalDisplayClear,   5);
-DEFN_SYSCALL2(malloc, 6, unsigned int, unsigned int);
 static void *syscalls[7] =
 {
-	&ULMonitorWriteChar,
-	&ULMonitorWriteHex,
-	&ULMonitorWriteDec,
-	&ULShutdown,
-	&ULReboot,
-	&HalDisplayClear,
-	&malloc
+	ULMonitorWriteChar,
+	ULMonitorWriteHex,
+	ULMonitorWriteDec,
+	ULShutdown,
+	ULReboot,
+	HalDisplayClear,
+	malloc
 };
 int num_syscalls = 7;
 
 void HalInitializeSyscalls()
 {
 	// Register our syscall handler.
-	//HalIdtSetEntry(0x80, (unsigned)HalIsrSyscall, 0x08, 0x8E);
 	HalSetIDTGate(0x80, (uint32_t) HalIsrSyscall, 0x08, 0x8E, 0, IDT_INTR32);
 }
 
@@ -68,25 +60,8 @@ void HalSyscallHandler(struct regs *r)
 	if (r->eax >= num_syscalls)
 		 return;
 
-	// Get the required syscall location.
-	void *location = syscalls[r->eax];
+	void (*syscall)(int, int, int, int, int);
 
-	// We don't know how many parameters the function wants, so we just
-	// push them all onto the stack in the correct order. The function will
-	// use all the parameters it wants, and we can pop them all back off afterwards.
-	int ret;
-	asm volatile (" \
-		push %1; \
-		push %2; \
-		push %3; \
-		push %4; \
-		push %5; \
-		call *%6; \
-		pop %%ebx; \
-		pop %%ebx; \
-		pop %%ebx; \
-		pop %%ebx; \
-		pop %%ebx; \
-	" : "=a" (ret) : "r" (r->edi), "r" (r->esi), "r" (r->edx), "r" (r->ecx), "r" (r->ebx), "r" (location));
-	r->eax = ret;
+	syscall = syscalls[r->eax];
+	syscall((int)r->edi, (int)r->esi, (int)r->edx, (int)r->ecx, (int)r->ebx);
 }
