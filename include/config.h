61aac8923613333f75ae8a8e1c63c140d13ba627
Ellen Marie Dash <me@duckie.co>
2010-06-04 20:53:07 -0400

    Fixed up syscalls, missed lien 15 of src/metodo/hal/i386/exceptions.asm (`push dword 0x20`)

--- include/config.h
+++ include/config.h
@@ -9,11 +9,14 @@
 /* Interrupt for syscalls:
  *   default is 0x21
  *   used to use 0x80 (got the idea from Linux)
+ * This must also be changed in HalIsrSyscall - line 15 in
+ * src/metodo/hal/i386/exceptions.asm
  */
-#define IA32_SYSCALL_INTERRUPT 0x21
+#define IA32_SYSCALL_INTERRUPT 0x20
+
 /* For the next one, a nice big thank you to inline assembly
  * see src/lib/krnllib/krnllib.c for details
  */
-#define IA32_SYSCALL_INTERRUPT_STR "0x21"
+#define IA32_SYSCALL_INTERRUPT_STR "0x20"
 
 #endif
