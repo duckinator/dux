ca94a13fc768bf39e645193af695c6f690efbe5c
Ellen Marie Dash <me@duckie.co>
2010-07-03 15:44:32 -0400

    Fixed up the scheduler, and found out that IRQs broke sometime in the past month. Horray. (I verified the version already on github did not work before committing this.)

--- src/metodo/hal/i386/scheduler.c
+++ src/metodo/hal/i386/scheduler.c
@@ -5,7 +5,7 @@ static int scheduler_enabled = 0;
 
 void HalSchedulerRunProcess(SchedulerProcess proc)
 {
-	printf("Switch process %i\n", CoSchedulerCurProcess());
+	printf("Switch process %i\n", proc.id);
 }
 
 void HalSchedulerEnable()
