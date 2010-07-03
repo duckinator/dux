eb3b22e9746d5ed71c1685196f8fe78ae1758e4c
Nick Markwell <nick@duckinator.net>
2010-07-03 15:44:32 -0400

    Fixed up the scheduler, and found out that IRQs broke sometime in the past month. Horray. (I verified the version already on github did not work before committing this.)

--- include/metodo/scheduler.h
+++ include/metodo/scheduler.h
@@ -6,14 +6,16 @@
 
 typedef struct SchedulerProcess_s {
   int used;
+  int id;
   struct proc_regs registers;
 } SchedulerProcess;
 
 void CoSchedulerHandler(void);
-uint32_t CoSchedulerCurProcess(void);
+SchedulerProcess CoSchedulerCurProcess(void);
 uint32_t CoSchedulerNumProcesses(void);
-uint8_t CoSchedulerFirstRun(void);
-void CoSchedulerNextProcess(void);
+void CoSchedulerFirstRun(void);
+uint32_t CoSchedulerNextProcessLoop(int begin, int end);
+uint32_t CoSchedulerNextProcess(void);
 void CoSchedulerSetNumProcesses(uint32_t num);
 
 void HalSchedulerRunProcess(SchedulerProcess proc);
--- src/metodo/core/scheduler.c
+++ src/metodo/core/scheduler.c
@@ -9,28 +9,30 @@ static uint32_t number_of_processes = 0;
 
 void CoSchedulerHandler(void)
 {
-	if (CoSchedulerFirstRun()) {
+	printf("CoSchedulerHandler\n");
+	if (scheduler_firstrun) {
+		CoSchedulerFirstRun();
+
 		processes[0].used = 1;
 		processes[100].used = 1;
 		number_of_processes = 101;
-	}
-	if (!processes[current_process].used) {
-		while (!processes[current_process].used) {
-			CoSchedulerNextProcess();
-		}
+
+		scheduler_firstrun = 0;
 	}
 
 	HalSchedulerRunProcess(processes[current_process]);
 
-	if (scheduler_firstrun)
-		scheduler_firstrun = 0;
-
 	CoSchedulerNextProcess();
 }
 
-uint32_t CoSchedulerCurProcess(void)
+uint32_t CoSchedulerCurProcessId(void)
 {
 	return current_process;
+}
+
+SchedulerProcess CoSchedulerCurProcess(void)
+{
+	return processes[current_process];
 }
 
 uint32_t CoSchedulerNumProcesses(void)
@@ -38,17 +40,49 @@ uint32_t CoSchedulerNumProcesses(void)
 	return number_of_processes;
 }
 
-uint8_t CoSchedulerFirstRun(void)
+void CoSchedulerFirstRun(void)
 {
-	return scheduler_firstrun;
+	uint32_t i;
+	printf("Updating process ids: ");
+	for(i = 0; i < sizeof(processes); i++) {
+		processes[i].id = i;
+	}
 }
 
-void CoSchedulerNextProcess(void)
+uint32_t CoSchedulerNextProcessLoop(int begin, int end)
 {
-	current_process++;
+	uint32_t i = 0;
+	
+	if (end == -1) {
+		end = sizeof(processes)-1;
+	}
 
-	if (current_process >= number_of_processes)
-		current_process = 0;
+	// Try processes with ids from begin to end
+	while(!processes[i].used) {
+		printf("%d is not runnnig, checking process %d...", i-1, i);
+		// Break if we're past the last process
+		if(i >= sizeof(end)) {
+			return -1; // Return -1 if no process found in the group
+		}
+		i++;
+	}
+	return i;
+}
+
+uint32_t CoSchedulerNextProcess(void)
+{
+	uint32_t result;
+	uint32_t last_process = current_process;
+
+	// Try processes current_process -> (sizeof(processes)-1)
+	result = CoSchedulerNextProcessLoop(current_process, -1);
+
+	// If result == -1, try again from 0 -> last_process
+	if(result == -1) {
+		result = CoSchedulerNextProcessLoop(0, last_process);
+	}
+
+	return result;
 }
 
 void CoSchedulerSetNumProcesses(uint32_t num)
