1ae70ed4b95982b63db769f3535a3b51aa78f33c
Ellen Marie Dash <me@duckie.co>
2010-07-04 03:02:47 -0400

    Stole nicer module handling from screwy :D

--- src/metodo/init/init.c
+++ src/metodo/init/init.c
@@ -17,6 +17,8 @@
 
 #include <metodo/elfloader.h>
 
+#include <metodo/modules.h>
+
 void InInitKernel(uint32_t magic, multiboot_info_t *multiboot)
 {
 	void *userland = NULL;
@@ -30,8 +32,10 @@ void InInitKernel(uint32_t magic, multiboot_info_t *multiboot)
 	}
 
 	/* mbd->flags */
-	int i;
+	int i, len;
 	module_t *module;
+	int current_module = 0;
+	modules = (Module*)kmalloc(sizeof(Module) * 1024);
 	if (mbd->flags>>3&1) {
 		module = (module_t*)mbd->mods_addr;
 		printf("We have %i modules.\n", mbd->mods_count);
@@ -39,15 +43,19 @@ void InInitKernel(uint32_t magic, multiboot_info_t *multiboot)
 			printf("\nFound module.\n");
 			printf("Module located at 0x%x-0x%x\n", module->mod_start, module->mod_end);
 			printf("Module name: %s\n", (char*)module->string);
-			if (strcmp((char*)(module->string), "/System/userland.exe") == 0){
-				userland = (void*) module->mod_start;
-				printf("\nUserland located at: 0x%x\n\n", userland);
-			}
+
+			len = sizeof(char) * strlen((char*)module->string)+1;
+			modules[current_module].name = (char*)kmalloc(len);
+			memcpy(modules[current_module].name, (char*)(module->string), len);
+			modules[current_module].exe = (void*) module->mod_start;
+			printf("Found executable %s at 0x%x\n", modules[current_module].name, modules[current_module].exe);
+			current_module++;
 		}
 	}
 
-	printf("userland: 0x%x\n", userland);
 	SystemTests();
+
+	userland = GetModule("/System/userland.exe");
 
 	/* Initialize pseudo-user mode */
 	if (userland != NULL) {
