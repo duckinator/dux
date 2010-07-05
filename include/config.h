f81b66a6250fd9c5d1a6f7f2162bea4b77d8883f
Nick Markwell <nick@duckinator.net>
2010-07-04 22:43:21 -0400

    Switching to interrupt 0xFF for syscalls to avoid any further annoyances. I failed at math before, 0x2F is IRQ 15, 0x30 is the first after exceptions/IRQs, but then there's PCI IRQs and whatnot as well

--- include/config.h
+++ include/config.h
@@ -7,16 +7,11 @@
 //#define PRINTF_USE_MM
 
 /* Interrupt for syscalls:
- *   default is 0x2F
+ *   default is 0xFF
  *   used to use 0x80 (got the idea from Linux)
- * This must also be changed in HalIsrSyscall - line 15 in
  * src/metodo/hal/i386/exceptions.asm
- *
- * Why the syscall interrupt is 0x2F:
- *   - 0x00 through 0x1F are exceptions
- *   - 0x20 through 0x2E are IRQs
- *   this leaves 0x2F through 0xFF (0xFF being the last interrupt)
+ * This must also be changed in HalIsrSyscall - line 15 in
  */
-#define IA32_SYSCALL_INTERRUPT 0x2F
+#define IA32_SYSCALL_INTERRUPT 0xFF
 
 #endif
