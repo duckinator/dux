6c5567ea0761ef66a18f5ba1e4f74a8e387a1e25
Ellen Marie Dash <me@duckie.co>
2010-07-15 02:26:45 -0400

    Renamed InInitKernel() -> InitKernel()

--- include/metodo/init.h
+++ include/metodo/init.h
@@ -4,6 +4,6 @@
 
 #include <multiboot.h>
 
-void InInitKernel(uint32_t magic, multiboot_info_t *mbd);
+void InitKernel(uint32_t magic, multiboot_info_t *mbd);
 
 #endif /* end of include guard: INIT_8MJWAGGR */
