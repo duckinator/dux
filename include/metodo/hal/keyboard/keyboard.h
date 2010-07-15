915de9c5785a64ff1861fd9f33de7dfaa7d10f4e
Ellen Marie Dash <me@duckie.co>
2010-07-15 03:25:59 -0400

    Keyboard driver overhaul :D

--- include/metodo/hal/keyboard/keyboard.h
+++ include/metodo/hal/keyboard/keyboard.h
@@ -6,6 +6,9 @@
 #define CAPSLOCK_LED 4
 
 typedef struct hal_key_info {
+	int ctrl_l, ctrl_r;
+	int alt_l, alt_r;
+	int shift_l, shift_r;
 	int action;
 	int scancode;
 	char key;
@@ -25,5 +28,13 @@ extern HalKeyInfo HalKeyboardResolveScancode_shift(HalKeyInfo keyinfo);
 extern void HalKeyboardInit();
 
 extern void HalKeyboardTest();
+
+// The following are a list of the 'make' codes.
+// We find break codes from these
+#define SHIFT_LEFT    0x2A
+#define SHIFT_RIGHT   0x36
+#define CAPSLOCK      0xBA
+#define ALT           0x38
+#define CONTROL       0x1D
 
 #endif
