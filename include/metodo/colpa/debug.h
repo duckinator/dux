118c2bb69c1f27c4d5932593b9664bb1a8c35d56
Ellen Marie Dash <me@duckie.co>
2010-07-15 03:40:45 -0400

    Moved dprint() and friends into their own file

--- include/metodo/colpa/debug.h
+++ include/metodo/colpa/debug.h
@@ -5,20 +5,6 @@
 #include <metodo/metodo.h>
 #include <metodo/hal/keyboard/keyboard.h>
 
-typedef enum {
-  NONE,
-	ALL,
-	FILESYSTEM,
-	MEMORY,
-	CONSOLE
-} debug_type;
- 
-static debug_type DEBUG_MODE = NONE;
-
-void dprint(debug_type type, char *str, const char *fmt, ...);
-void set_debug_mode(debug_type type);
-debug_type get_debug_mode(void);
-
 void KernDebug();
 
 
---
+++ include/metodo/colpa/dprint.h
@@ -0,0 +1,21 @@
+#ifndef DPRINT_H
+#define DPRINT_H
+
+#include <system.h>
+#include <metodo/metodo.h>
+
+typedef enum {
+  NONE,
+	ALL,
+	FILESYSTEM,
+	MEMORY,
+	CONSOLE
+} debug_type;
+ 
+static debug_type DEBUG_MODE = NONE;
+
+void dprint(debug_type type, char *str, const char *fmt, ...);
+void set_debug_mode(debug_type type);
+debug_type get_debug_mode(void);
+
+#endif
