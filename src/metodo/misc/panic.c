49c6c3fc118d1ad8dd596110763f1c97d0b42d2d
Nick Markwell <nick@duckinator.net>
2010-07-19 19:55:58 -0400

    Typoed. UDDUD -> UDUDD

--- src/metodo/misc/panic.c
+++ src/metodo/misc/panic.c
@@ -42,7 +42,7 @@ void _panic(char *text, char *filename, char *line)
 	HalInitDisplay();
 	HalDisplaySetAttr(0x4f);
 	HalDisplayClear();
-	printf("\n**** UDDUD ***\n\n%s\n\n", text);
+	printf("\n**** UDUDD ***\n\n%s\n\n", text);
 	printf("File: %s\nLine: %s\n\n", filename, line);
 	stack_dump();
 	asm("cli");
