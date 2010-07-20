a58866d31ea91c5fa95886462916dac0c177c5de
Nick Markwell <nick@duckinator.net>
2010-07-19 20:52:47 -0400

    _panic(char *text, char *filename, char *line) -> panic(char *text, char *filename, int line);  Changed panic() macro appropriately

--- include/system.h
+++ include/system.h
@@ -24,8 +24,8 @@ extern void usleep();*/
 
 
 /* Handle awesome kernel panic implementation */
-void _panic(char *text, char *filename, char *line);
-#define panic(message) _panic(message, __FILE__, MACRO_STRING(__LINE__))
+void _panic(char *text, char *filename, int line);
+#define panic(message) _panic(message, __FILE__, __LINE__)
 
 void stop(int error, int argc, ...);
 #define assert(a) if (!(a)) assert_dowork(__FILE__, __LINE__)
