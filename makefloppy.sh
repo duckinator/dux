6e00a7ee154623ae38937000523473d9b263ea90
Ellen Marie Dash <me@duckie.co>
2010-07-30 18:14:17 -0400

    added Dux.img to .gitignore, added makefloppy.sh

---
+++ makefloppy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+dd bs=1024 count=1440 if=/dev/zero of=fdd.img
+mkfs.msdos fdd.img
+
+mkdir mnt
+losetup /dev/loop1 fdd.img
+mount /dev/loop1 mnt
+cp -r isofs/* mnt
+#cp $3.map $1/$3.map
+umount /dev/loop1
+losetup -d /dev/loop1
+rmdir mnt
+dd bs=512 count=1 if=isofs/boot/boot of=Dux.img conv=notrunc
