029e44b15468b04cbe0b413b97289c69e3109bc4
Nick Markwell <nick@duckinator.net>
2010-07-30 21:34:34 -0400

    Fixed makefloppy.sh

--- makefloppy.sh
+++ makefloppy.sh
@@ -1,14 +1,30 @@
 #!/bin/sh
 
 dd bs=1024 count=1440 if=/dev/zero of=Dux.img
-mkfs.msdos fdd.img
 
-mkdir mnt
-losetup /dev/loop1 fdd.img
-mount /dev/loop1 mnt
-cp -r isofs/* mnt
-#cp $3.map $1/$3.map
+mkfs.msdos Dux.img
+
+
+# Create ./floppy
+mkdir floppy
+
+# Enable/configure loopback 1
+losetup /dev/loop1 Dux.img
+
+# Mount loopback 1
+mount /dev/loop1 floppy
+
+# Copy everything from isofs/ to floppy/
+cp -r isofs/* floppy/
+
+# Unmount loopback 1
 umount /dev/loop1
+
+# Disable loopback 1
 losetup -d /dev/loop1
-rmdir mnt
+
+# Delete ./floppy
+rmdir floppy
+
+# Copy bootloader
 dd bs=512 count=1 if=isofs/boot/boot of=Dux.img conv=notrunc
