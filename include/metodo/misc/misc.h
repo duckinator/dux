bba2fe276f68996b02cc8d9ccef7efaf4b7444a0
Ellen Marie Dash <me@duckie.co>
2010-07-30 21:43:46 -0400

    Fixed issue with redeclaration of strlen()

--- include/metodo/misc/misc.h
+++ include/metodo/misc/misc.h
@@ -4,5 +4,5 @@
 void *memcpy(void *dest, void *src, unsigned int count);
 extern void *memset(void *dest, unsigned char val, unsigned int count);
 unsigned short *memsetw(unsigned short *dest, unsigned short val, unsigned int count);
-int strlen(const char *str);
+
 #endif /* end of include guard: MISC_H */
