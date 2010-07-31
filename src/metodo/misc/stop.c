4fe3db575f08bfde4d25789d39a73e0318a3633a
Nick Markwell <nick@duckinator.net>
2010-07-31 02:54:26 -0400

    Fixed warnings in src/metodo/misc/stop.c

--- src/metodo/misc/stop.c
+++ src/metodo/misc/stop.c
@@ -21,12 +21,12 @@ void KrnlStop(uint32_t error, uint32_t arg1, uint32_t arg2, uint32_t arg3, uint3
 	HalDisplaySetAttr(0x4f);
 	HalDisplayClear();
 
-	HalDisplayString("An error has occured and Dux has been shutdown to prevent damage.\n\n");
+	HalDisplayString((char*)"An error has occured and Dux has been shutdown to prevent damage.\n\n");
 
 	printf("STOP: %x (%x, %x, %x, %x)\n", error, arg1, arg2, arg3, arg4);
  
 #ifdef DEBUG
-	HalDisplayString("BREAK\n");
+	HalDisplayString((char*)"BREAK\n");
 	HalBreak();
 #else
 	HalShutdown();
