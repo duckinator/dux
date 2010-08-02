67b137685c3a8bcce6a2d6713d5ffed99043fd5e
Ellen Marie Dash <me@duckie.co>
2010-08-02 04:21:43 -0400

    Massively overhauled driver system, we now have the serial module as a *driver module*! I'll work on having them automatically loaded + ran tomorrow! \o/

--- include/driver/i386/vga.h
+++ include/driver/i386/vga.h
@@ -1,7 +1,7 @@
-#ifndef HAL_VGA_H
-#define HAL_VGA_H
+#ifndef DRIVER_VGA_H
+#define DRIVER_VGA_H
 
-#include <metodo/metodo.h>
+#include <driver/driver.h>
 
 void VgaDisplaySetAttr(uint8_t lattr);
 void VgaDisplayClear(void);
@@ -10,7 +10,7 @@ void VgaDisplayChar(char c);
 void VgaDisplaySpot(uint8_t s, uint8_t row, uint8_t col);
 void VgaDisplayHideCursor();
 void VgaDisplayCursorPosition(uint8_t _row, uint8_t _col);
-void VgaInitDisplay(void);
+void VgaDisplayInit(void);
 void VgaDisableDisplay(void);
 int VgaIsDisplayOn(void);
 
