0b33d580897880b199584baa4ee2dfa0b8b68744
Ellen Marie Dash <me@duckie.co>
2010-08-04 02:00:58 -0400

    Define size_t properly for x86-64 and i686

--- include/stddef.h
+++ include/stddef.h
@@ -1,7 +1,13 @@
 #ifndef STDDEF_H
 #define STDDEF_H
 
-typedef unsigned int size_t;
+#ifdef __x86_64
+	// 8 bytes for 64bit
+	typedef unsigned long int size_t;
+#else
+	// 4 bytes for 32bit
+	typedef unsigned int size_t;
+#endif
 
 #define NULL ((void*)(0))
 
