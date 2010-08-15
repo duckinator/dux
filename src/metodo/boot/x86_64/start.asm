75d97f02b21a80183768d125dff1295315ec9ebc
Nick Markwell <nick@duckinator.net>
2010-08-14 22:07:14 -0400

    Removed unused GDT in i386/start.asm and x86_64/start.asm (yes, i will start to actually port the x86_64 one soon)

--- src/metodo/boot/x86_64/start.asm
+++ src/metodo/boot/x86_64/start.asm
@@ -16,49 +16,6 @@ MultiBootHeader:
 	dd FLAGS
 	dd CHECKSUM
 
-gdt:
-
-gdt_null:
-	dq 0
-
-gdt_code:
-	dw 0xFFFF    ; first 16 bits of segment limiter
-	dw 0         ; first 16 bits of base address
-	db 0         ; next 8 bits of base address
-	db 10011010b ; code segment, readable, nonconforming
-	db 11001111b ; ganular, last 4 bits of segment limiter
-	db 0         ; final 8 bits of base address
-
-gdt_data:
-	dw 0xFFFF
-	dw 0		; first 16 bits of base address
-	db 0		; next 8 bits of base address
-	db 10010010b	; data segment, writable, extends downwards
-	db 11001111b	; big
-	db 0		; final 8 bits of base address
-
-gdt_user_code:   ; user mode code segment
-    dw 0xFFFF
-    dw 0
-    db 0
-    db 11111010b
-    db 11001111b
-    db 0
-
-gdt_user_data:   ; user mode data segment
-    dw 0xFFFF
-    dw 0
-    db 0
-    db 11110010b
-    db 11001111b
-    db 0
-
-gdt_end:
-
-gdt_desc:
-	dw gdt_end - gdt - 1
-	dd gdt
-
 ; reserve initial kernel stack space
 STACKSIZE equ 0x4000			; that's 16k.
 
