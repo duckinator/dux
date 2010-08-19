5eb4aeb14c70570cee8184aac788991e91e81a37
Nick Markwell <nick@duckinator.net>
2010-08-18 23:49:36 -0400

    DUX_i386 and DUX_I386 or DUX_x86_64 and DUX_X86_64 are defined now depending on the arch :)

--- tools/buildinfo.sh
+++ tools/buildinfo.sh
@@ -5,4 +5,5 @@ ARCH=$(echo "$2" | tr [a-z] [A-Z])
 echo  "#define SCM_REV \"$(git show | head -n1 | cut -b9-)\""
 echo  "#define DUX_BUILDTYPE \"$1\""
 echo  "#define DUX_ARCH \"$2\""
+echo  "#define DUX_$2"
 echo  "#define DUX_$ARCH"
