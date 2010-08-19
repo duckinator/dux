7197f985d469fd49866f46903aaffdf6e0722dda
Nick Markwell <nick@duckinator.net>
2010-08-19 00:24:07 -0400

    Fixed buildinfo.sh

--- tools/buildinfo.sh
+++ tools/buildinfo.sh
@@ -2,8 +2,15 @@
 
 ARCH=$(echo "$2" | tr [a-z] [A-Z])
 
+echo  "#ifndef DUX_BUILDINFO_H"
+echo  "#define DUX_BUILDINFO_H"
 echo  "#define SCM_REV \"$(git show | head -n1 | cut -b9-)\""
 echo  "#define DUX_BUILDTYPE \"$1\""
 echo  "#define DUX_ARCH \"$2\""
+echo  "#ifndef $2"
 echo  "#define $2"
+echo  "#endif"
+echo  "#ifndef $ARCH"
 echo  "#define $ARCH"
+echo  "#endif"
+echo  "#endif"
