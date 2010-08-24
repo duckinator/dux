f9ab878ea47d9d92c0f847a4b918315149313177
Nick Markwell <nick@duckinator.net>
2010-08-24 18:29:15 -0400

    noreturn should be in modifiers.h, not macros.h

--- include/macros.h
+++ include/macros.h
@@ -4,6 +4,4 @@
 #define STRING(x) #x
 #define MACRO_STRING(x) STRING(x)
 
-#define noreturn __attribute__((noreturn)) void // Specify that a function doesn't return
-
 #endif /* end of include guard: MACROS_H */
