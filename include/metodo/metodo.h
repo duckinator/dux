e5132556eea346f5e4a40b7924fa2ce5043f4d46
Nick Markwell <nick@duckinator.net>
2010-09-06 13:59:04 -0400

    Removed references in include/metodo/metodo.h to monitor_* functions which haven't existed for ages

--- include/metodo/metodo.h
+++ include/metodo/metodo.h
@@ -14,12 +14,6 @@
 
 #include <metodo/misc/panic.h>
 
-extern void monitor_clear();
-extern void monitor_switch_pages(unsigned int a, unsigned int b);
-extern unsigned int monitor_get_cursor_x();
-extern unsigned int monitor_get_cursor_y();
-extern void monitor_set_cursor(unsigned int x, unsigned int y);
-
 //extern void HalTimerSleep();
 
 multiboot_info_t *mbd;
