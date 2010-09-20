5a437d053540bdc85858766e9120e7e74df15a6b
Ellen Marie Dash <me@duckie.co>
2010-09-20 12:44:36 -0400

    YES!!!!!! We finally have most of a working scheduler. As soon as HalSchedulerRunProcess() works, we'll have a full, working scheduler :D

--- src/metodo/core/scheduler.c
+++ src/metodo/core/scheduler.c
@@ -1,39 +1,36 @@
+#include <config.h>
 #include <metodo/metodo.h>
 #include <metodo/core/scheduler.h>
 
-SchedulerProcess **processes;
+SchedulerProcess *first_process;
+SchedulerProcess *last_process;
 
-int32_t current_process_id = 0;
+SchedulerProcess *current_process;
 uint8_t scheduler_firstrun = 1;
-int32_t number_of_processes = 0;
+uint32_t number_of_processes = 0;
 
 void CoSchedulerHandler(void)
 {
-
-	if (scheduler_firstrun)
-		*processes = kmalloc(sizeof(SchedulerProcess)*1024);
-
 	if (scheduler_firstrun) {
-		processes[0]->used = 1;
-		processes[100]->used = 1;
-		number_of_processes = 101;
+		first_process = CoSchedulerAllocateProcess();
+		first_process->next = first_process;
+		first_process->prev = first_process;
+
+		last_process = first_process;
+		current_process = first_process;
+
+		CoSchedulerAllocateProcess();
 
 		scheduler_firstrun = 0;
 	}
 
-	HalSchedulerRunProcess(processes[current_process_id]);
-
-	current_process_id = CoSchedulerNextProcess();
-}
-
-int32_t CoSchedulerCurProcessId(void)
-{
-	return current_process_id;
+	HalSchedulerRunProcess(current_process);
+	current_process = CoSchedulerNextProcess();
 }
 
 SchedulerProcess *CoSchedulerCurProcess(void)
 {
-	return processes[current_process_id];
+	return current_process;
 }
 
 int32_t CoSchedulerNumProcesses(void)
@@ -41,58 +38,51 @@ int32_t CoSchedulerNumProcesses(void)
 	return number_of_processes;
 }
 
-int32_t CoSchedulerNextProcessLoop(int32_t begin, int32_t end)
+SchedulerProcess *CoSchedulerPrevProcess(void)
 {
-	int32_t i;
-
-	if (end == -1) {
-		end = number_of_processes;
-	}
-
-	if (begin == end) {
-		if(end > number_of_processes) {
-			end = number_of_processes;
-		} else {
-			begin = 0;
-			end = number_of_processes;
-		}
-	}
-
-	i = begin;
-
-	// Try processes with ids from begin to end
-	while(!processes[i]->used) {
-		if(i > end) // If we are past the last process in the group, return -1
-			return -1;
-		i++;
-	}
-
-	return i;
+	current_process = current_process->prev;
+	return current_process;
 }
 
-int32_t CoSchedulerNextProcess(void)
+SchedulerProcess *CoSchedulerNextProcess(void)
 {
-	int32_t result;
-	int32_t last_process = current_process_id;
-
-	// Try processes current_process_id -> (number_of_processes-1)
-	result = CoSchedulerNextProcessLoop(last_process+1, -1);
-
-	// If result == -1, try again from 0 -> last_process
-	if(result == -1) {
-		result = CoSchedulerNextProcessLoop(0, last_process);
-	}
-
-	// If result is still -1, either something exploded or there's no processes
-	if(result == -1) {
-		printf("\n\nNo processes");
-		while(1);
-	}
-
-	return result;
+	current_process = current_process->next;
+	return current_process;
 }
 
-void CoSchedulerSetNumProcesses(int32_t num)
+SchedulerProcess *CoSchedulerAllocateProcess()
 {
-	number_of_processes = num;
+	SchedulerProcess *old_last;
+	SchedulerProcess *new = kmalloc(sizeof(SchedulerProcess));
+	memset(new, 0, sizeof(SchedulerProcess));
+
+	old_last = last_process;
+	last_process = new;
+	old_last->next = last_process;
+	last_process->prev = old_last;
+	last_process->next = first_process;
+	new->used = 0;
+
+	number_of_processes++;
+	new->id = number_of_processes;
+	return new;
+}
+
+int CoSchedulerDestroyProcess(SchedulerProcess *proc)
+{
+	if((proc == first_process) && (proc == last_process)) {
+		panic("No processes running.");
+	} else if(proc == first_process) {
+		first_process = first_process->next;
+	} else if(proc == last_process) {
+		last_process = last_process->prev;
+	}
+	proc->next->prev = proc->prev;
+	proc->prev->next = proc->next;
+	free(proc);
+
+	if(((SchedulerProcess*)proc)->used == 1)
+		return -1;
+	else
+		return 0;
 }
