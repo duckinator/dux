3430ba5a78708d95a9e2381f502da8c22c0d6987
Ellen Marie Dash <me@duckie.co>
2011-02-25 20:55:58 -0500

    Fixed old paths

--- include/metodo/amd64/hal.h
+++ include/metodo/amd64/hal.h
@@ -1,7 +1,7 @@
 #ifndef HAL_X86_64_H
 #define HAL_X86_64_H
 
-#include <metodo/hal/x86_64/idt.h>
+#include <metodo/amd64/hal/idt.h>
 
 /* Interrupts */
 void HalDisableInterrupts(void);
@@ -10,10 +10,10 @@ void HalEnableInterrupts(void);
 void HalDisableNMI(void);
 void HalEnableNMI(void);
 
-#include <metodo/hal/x86_64/exceptions.h>
-#include <metodo/hal/x86_64/ports.h>
-#include <metodo/hal/x86_64/isr.h>
-#include <metodo/hal/x86_64/syscall.h>
+#include <metodo/amd64/hal/exceptions.h>
+#include <metodo/amd64/hal/ports.h>
+#include <metodo/amd64/hal/isr.h>
+#include <metodo/amd64/hal/syscall.h>
 
 #include <driver/x86_64/timer.h>
 #include <driver/x86_64/keyboard/keyboard.h>
