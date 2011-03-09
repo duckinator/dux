b8176e2ca567140a506b7bafc955010c15db6a77
Nick Markwell <nick@duckinator.net>
2011-03-09 00:35:02 -0500

    Moved a ton of i386/amd64 dupes to x86

--- include/driver/i386/timer.h
+++
@@ -1,8 +0,0 @@
-#ifndef HAL_TIMER_H
-#define HAL_TIMER_H
-
-extern void HalTimerHandler(struct regs *r);
-extern unsigned int HalGetTicks();
-extern void HalTimerInit();
-
-#endif /* end of include guard: HAL_TIMER_H */
--- src/metodo/driver/x86/timer.c
+++ src/metodo/driver/x86/timer.c
@@ -1,5 +1,5 @@
 #include <driver/driver.h>
-#include <driver/i386/timer.h>
+#include <driver/x86/timer.h>
 #include <metodo/core/scheduler.h>
 
 unsigned int ticks = 0;
