e17191c55e7f1866ab6065bfd4bdada4901b24cc
Nick Markwell <nick@duckinator.net>
2011-03-12 20:50:58 -0500

    removed redundant HalEnableInterrupts/HalDisableInterrupts and HalEnableNMI/HalDisableNMI declarations

--- include/metodo/amd64/hal.h
+++ include/metodo/amd64/hal.h
@@ -3,13 +3,6 @@
 
 #include <metodo/amd64/hal/idt.h>
 
-/* Interrupts */
-void HalDisableInterrupts(void);
-void HalEnableInterrupts(void);
-
-void HalDisableNMI(void);
-void HalEnableNMI(void);
-
 #include <metodo/amd64/hal/exceptions.h>
 #include <metodo/amd64/hal/ports.h>
 #include <metodo/amd64/hal/isr.h>
--- include/metodo/i386/hal.h
+++ include/metodo/i386/hal.h
@@ -3,13 +3,6 @@
 
 #include <metodo/i386/hal/idt.h>
 
-/* Interrupts */
-void HalDisableInterrupts(void);
-void HalEnableInterrupts(void);
-
-void HalDisableNMI(void);
-void HalEnableNMI(void);
-
 #include <metodo/i386/hal/exceptions.h>
 #include <metodo/i386/hal/ports.h>
 #include <metodo/i386/hal/isr.h>
