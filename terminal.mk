f0e2018987579f652bf917dc23a3d491b8e5cc1e
Nick Markwell <nick@duckinator.net>
2011-03-14 04:34:53 -0400

    Changed filename in [COMPILE]/[ASSEMBLE] lines to red, since it's more indicative of the fact that if you see it, it's almost guaranteed to be an error (unless your terminal fails it up a bit)

--- terminal.mk
+++ terminal.mk
@@ -1,4 +1,5 @@
 COLOR_GREEN  = "\\033[0\;32m" # Set console color to green
+COLOR_RED    = "\\033[0\;31m" # Set console color to red
 COLOR_BLUE   = "\\033[0\;34m" # Set console color to blue
 COLOR_RESET  = "\\033[0m"    # Set console color to previously used one
 
@@ -8,5 +9,5 @@ UP_ONE_LINE  = "\\033[1A"    # Move cursor up one line
 
 MESSAGE_PRE  = "${UP_ONE_LINE}\\r${CLEAR_TO_EOL}"
 
-STATUS       = echo -e "${MESSAGE_PRE}${COLOR_GREEN}\\b\\b[$(1)]${COLOR_RESET} $(dir $(2))${COLOR_BLUE}\\b$(notdir $(2))${COLOR_RESET}"
+STATUS       = echo -e "${MESSAGE_PRE}${COLOR_GREEN}\\b\\b[$(1)]${COLOR_RESET} $(dir $(2))${COLOR_RED}\\b$(notdir $(2))${COLOR_RESET}"
 
