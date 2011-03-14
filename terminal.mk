9fec10aea3efe14c34d43ca1b07ddc3a9e061bce
Ellen Marie Dash <me@duckie.co>
2011-03-14 01:22:52 -0400

    Woops...forgot terminal.mk

---
+++ terminal.mk
@@ -0,0 +1,9 @@
+COLOR_GREEN = "\\033[0\;32m" # Set console color to green
+COLOR_RESET = "\\033[0m"    # Set console color to previously used one
+
+CLEAR_TO_EOL = "\\033[K"    # Clear to end of line
+
+UP_ONE_LINE = "\\033[1A"    # Move cursor up one line
+
+MESSAGE_PRE = "${UP_ONE_LINE}\\r${CLEAR_TO_EOL}"
+
