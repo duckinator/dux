9e5f9b61d3d3c13b0e9506b2fc83b4406d21a799
Ellen Marie Dash <me@duckie.co>
2011-03-14 04:25:10 -0400

    More tweaks to pretty makefile output

--- terminal.mk
+++ terminal.mk
@@ -1,11 +1,12 @@
-COLOR_GREEN = "\\033[0\;32m" # Set console color to green
-COLOR_RESET = "\\033[0m"    # Set console color to previously used one
+COLOR_GREEN  = "\\033[0\;32m" # Set console color to green
+COLOR_BLUE   = "\\033[0\;34m" # Set console color to blue
+COLOR_RESET  = "\\033[0m"    # Set console color to previously used one
 
 CLEAR_TO_EOL = "\\033[K"    # Clear to end of line
 
-UP_ONE_LINE = "\\033[1A"    # Move cursor up one line
+UP_ONE_LINE  = "\\033[1A"    # Move cursor up one line
 
-MESSAGE_PRE = "${UP_ONE_LINE}\\r${CLEAR_TO_EOL}"
+MESSAGE_PRE  = "${UP_ONE_LINE}\\r${CLEAR_TO_EOL}"
 
-STATUS = echo -e "${MESSAGE_PRE}${COLOR_GREEN}[$(1)]${COLOR_RESET} $(2)"
+STATUS       = echo -e "${MESSAGE_PRE}${COLOR_GREEN}\\b\\b[$(1)]${COLOR_RESET} $(dir $(2))${COLOR_BLUE}\\b$(notdir $(2))${COLOR_RESET}"
 
