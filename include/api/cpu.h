6ebd4e5e32313a82fab1b8a30c9f92b1456c8360
Nick Markwell <nick@duckinator.net>
2011-05-26 03:25:55 -0400

    more CPU API dummy functions and such

--- include/api/cpu.h
+++ include/api/cpu.h
@@ -1,21 +1,23 @@
 #ifndef API_CPU_H
 #define API_CPU_H
 
+#if defined(DUX_I386) || defined(DUX_AMD64)
+	#include <metodo/x86/cpuid.h>
+#endif
+
 typedef struct {
 	char family[15]; // Brand and such: "Intel P4", "AMD K8", etc
 } CPUInfo_t;
 
-/*
 typedef struct {
 } CPU_features_t;
-*/
 
 char *CPUBrand();
-//char *CPUShortBrand();
+char *CPUShortBrand();
 char *CPUFamily();
 char *CPUModel();
 int CPUCount();
-//CPU_features_t *CPUFeatures();
+CPU_features_t *CPUFeatures();
 int CPUCacheSize();
 char *CPUSerial();
 
