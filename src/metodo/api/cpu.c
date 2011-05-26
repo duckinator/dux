fe7fc4b8ea2e567ab65c3eee55e5baf68f7e03c1
Ellen Marie Dash <me@duckie.co>
2011-05-26 03:19:40 -0400

    more thorough dummy functions for the CPU API D;

--- src/metodo/api/cpu.c
+++ src/metodo/api/cpu.c
@@ -1,49 +1,51 @@
+#include <metodo/metodo.h>
 #include <api/cpu.h>
 
-/*
 char *CPUBrand()
-{ 
+{
+	// FIXME: Dummy CPU API
+	char *ret = kmalloc(1);
+	return ret;
 }
-*/
 
-/*
 char *CPUShortBrand()
 {
+	// FIXME: Dummy CPU API
+	char *ret = kmalloc(1);
+	return ret;
 }
-*/
 
-/*
 char *CPUFamily()
 {
+	// FIXME: Dummy CPU API
+	char *ret = kmalloc(1);
+	return ret;
 }
-*/
 
-/*
 char *CPUModel()
 {
+	// FIXME: Dummy CPU API
+	char *ret = kmalloc(1);
+	return ret;
 }
-*/
 
-/*
 int CPUCount()
 {
+	return 0;
 }
-*/
 
-/*
-CPU_features_t *CPUFeatures()
+/*CPU_features_t *CPUFeatures()
 {
-}
-*/
+}*/
 
-/*
 int CPUCacheSize()
 {
+	return 0;
 }
-*/
 
-/*
 char *CPUSerial()
 {
+	// FIXME: Dummy CPU API
+	char *ret = kmalloc(1);
+	return ret;
 }
-*/
