abc27bf6358f4f28d6bb23e482cf776e979bdc1a
Ellen Marie Dash <me@duckie.co>
2012-07-30 11:26:07 -0400

    Copy everything to correct place. Still won't compile.

--- makeiso.sh
+++ makeiso.sh
@@ -13,7 +13,12 @@ fi
 
 mkdir -p isofs/System
 
-mv metodo.exe isofs/System/
+mv src/metodo/metodo.exe isofs/System
+mv src/user/user.exe     isofs/System
+
+for x in `find -name '*.lib'`; do
+  cp $x isofs/System
+done
 
 $isocmd -R -b boot/grub/stage2_eltorito -no-emul-boot -boot-load-size 4 -boot-info-table -input-charset utf-8 -o dux.iso isofs
 
