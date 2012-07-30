a222df44d2486896f1dc4476b7c3bf5c6bd3f94f
Nick Markwell <nick@duckinator.net>
2012-07-30 11:13:25 -0400

    This should fix the random '-e's everywhere on Ubuntu.

--- terminal.mk
+++ terminal.mk
@@ -1,3 +1,5 @@
+
+
 COLOR_GREEN  = "\\033[0\;32m" # Set console color to green
 COLOR_RED    = "\\033[0\;31m" # Set console color to red
 COLOR_BLUE   = "\\033[0\;34m" # Set console color to blue
@@ -10,5 +12,5 @@ UP_ONE_LINE  = "\\033[1A"    # Move cursor up one line
 MESSAGE_PRE  = "${UP_ONE_LINE}\\r${CLEAR_TO_EOL}"
 MESSAGE_PRE  = ""
 
-STATUS       = echo -e "${MESSAGE_PRE}${COLOR_GREEN}\\b\\b[$(1)]${COLOR_RESET} $(dir $(2))${COLOR_RED}\\b$(notdir $(2))${COLOR_RESET}"
+STATUS       = ${ECHO_E} "${MESSAGE_PRE}${COLOR_GREEN}\\b\\b[$(1)]${COLOR_RESET} $(dir $(2))${COLOR_RED}\\b$(notdir $(2))${COLOR_RESET}"
 
