ad55400248a57ba6e4efe4cb343d34fed69112e9
Nick Markwell <nick@duckinator.net>
2012-07-31 19:41:52 -0400

    Some more work on module loading. Can't do anything related to deps until we have IPC of some sort.

--- include/module.h
+++ include/module.h
@@ -1,9 +1,11 @@
 #ifndef MODULE_H
 #define MODULE_H
 
+#include <metodo/misc/modules.h>
+
 #define MODULE(name, ...) \
-	int name##__moduleInit();\
-/*	void _start() { RegisterModuleCallback(name##__ModuleInit, __VA_ARGS__); }*/\
+	int name##__ModuleInit();\
+	/*void _start() { RegisterModuleCallback(name##__ModuleInit, #name, ##__VA_ARGS__); }*/\
 	void _start() { name##__ModuleInit(); };\
 	int name##__ModuleInit()
 
