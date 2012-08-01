2b8b400313a10e9971ed723577131856934a7a3b
Nick Markwell <nick@duckinator.net>
2012-07-31 23:51:32 -0400

    Move modules to individual directories so ACPI can be in its own dir without being special-cased.

--- modules.mk
+++ modules.mk
@@ -4,8 +4,8 @@ modules: metodo.exe test_module.exe uart.exe
 	@mkdir -p isofs/Modules
 	@cp src/modules/test_module.exe isofs/Modules/
 
-test_module.exe: krnllib.lib libc.lib src/modules/test_module.o
-	@${LD} -o src/modules/test_module.exe ${LDFLAGS} -Ttext 0x200000 src/modules/test_module.o -Lsrc/lib/krnllib src/lib/krnllib/krnllib.lib -Lsrc/lib/libc src/lib/libc/libc.lib
+test_module.exe: krnllib.lib libc.lib src/modules/test_module/test_module.o
+	@${LD} -o src/modules/test_module/test_module.exe ${LDFLAGS} -Ttext 0x200000 src/modules/test_module/test_module.o -Lsrc/lib/krnllib src/lib/krnllib/krnllib.lib -Lsrc/lib/libc src/lib/libc/libc.lib
 
-uart.exe: krnllib.lib libc.lib hal.lib src/modules/uart.o
-	@${LD} -o src/modules/uart.exe ${LDFLAGS} -Ttext 0x200000 src/modules/uart.o -Lsrc/lib/krnllib src/lib/krnllib/krnllib.lib -Lsrc/lib/libc src/lib/libc/libc.lib -Lsrc/metodo/hal src/metodo/hal/hal.lib
+uart.exe: krnllib.lib libc.lib hal.lib src/modules/uart/uart.o
+	@${LD} -o src/modules/uart/uart.exe ${LDFLAGS} -Ttext 0x200000 src/modules/uart/uart.o -Lsrc/lib/krnllib src/lib/krnllib/krnllib.lib -Lsrc/lib/libc src/lib/libc/libc.lib -Lsrc/metodo/hal src/metodo/hal/hal.lib
