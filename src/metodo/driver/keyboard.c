9796b086e8686060b9899f86378d99a70c86c39b
Nick Markwell <nick@duckinator.net>
2012-07-31 22:29:50 -0400

    Using @programble's keyboard driver from proxos.

--- src/metodo/driver/keyboard.c
+++ src/metodo/driver/keyboard.c
@@ -1,29 +1,66 @@
 #include <driver/driver.h>
 #include <driver/keyboard/keysym.h>
 
-#define SCANCODE(x) ( (scancode == x) || (scancode == (x + 0x80))) // Does this match either the make or break code?
-#define KEYINFO(x) keyinfo->x = x // KEYINFO(foo) is the same as keyinfo->foo=foo;
+int shift_l  = 0,
+	shift_r  = 0,
+	capslock = 0,
+	numlock  = 0,
+	alt_l	= 0,
+	alt_r	= 0,
+	ctrl_l   = 0,
+	ctrl_r   = 0,
+	shift	= 0,
+	alt	  = 0,
+	ctrl	 = 0,
+	escaped  = 0;
 
-unsigned char buf[0x1000];
-unsigned char *buffer;
-unsigned char *origbuffer;
-int shift_l = 0;
-int shift_r = 0;
-int capslock = 0;
-int numlock = 0;
-int alt_l = 0;
-int alt_r = 0;
-int ctrl_l = 0;
-int ctrl_r = 0;
-int escaped = 0;
-
-int vt_visible = 0; // VT 0 is visible
+HalKeyboardEventQueue *HalKeyboardEventQueue_head = NULL;
+HalKeyboardEventQueue *HalKeyboardEventQueue_tail = NULL;
 
 void HalKeyboardHandler(UNUSED struct regs *r)
 {
-	unsigned int scancode = HalInPort(0x60);
+	uint8_t keyCode = HalInPort(0x60);
 
-	*++buffer = (unsigned char)scancode;
+	HalKeyboardEvent *event = kmalloc(sizeof(HalKeyboardEvent));
+
+	if (keyCode & 0x80) {
+		event->type = HalKeyboardEventType_up;
+		keyCode = keyCode & ~0x80;
+	} else {
+		event->type = HalKeyboardEventType_down;
+	}
+
+	event->code = keyCode;
+	event->character = (shift_l || shift_r) ? keysym_us_shift[keyCode] : keysym_us[keyCode];
+	event->shift_l = shift_l;
+	event->shift_r = shift_r;
+	event->shift = shift_l || shift_r;
+	event->ctrl_l = ctrl_l;
+	event->ctrl_r = ctrl_r;
+	event->ctrl = ctrl;
+	event->alt_l = alt_l;
+	event->alt_r = alt_r;
+	event->alt = alt;
+
+	switch (event->code)
+	{
+	case 0x2A:
+		shift_l = !event->type;
+		break;
+	case 0x36:
+		shift_r = !event->type;
+		break;
+	case 0x1D:
+		// TODO: Handle LCtrl/RCtrl separately.
+		ctrl = ctrl_l = ctrl_r = !event->type;
+		break;
+	case 0x38:
+		// TODO: Handle LAlt/RAlt separately.
+		alt = alt_l = alt_r = !event->type;
+		break;
+	}
+
+	HalKeyboardEventQueue_enqueue(event);
 }
 
 void HalKeyboardLeds(uint8_t status){
@@ -44,116 +81,64 @@ int HalKeyboardNumlock()
 	return numlock;
 }
 
-char HalKeyboardHasInput()
+void HalKeyboardEventQueue_enqueue(HalKeyboardEvent *event)
 {
-	return buffer > origbuffer;
-}
-
-HalKeyInfo *HalKeyboardRead()
-{
-	HalKeyInfo *keyinfo = kmalloc(sizeof(HalKeyInfo));
-	int scancode;
-
-	while (buffer <= origbuffer); // wait for input
-
-	scancode = *buffer-- & 0xFF;
-
-	keyinfo->scancode = scancode;
-	keyinfo->key = (char)scancode;
-
-	if ( scancode & 0x80 )
-		// Released ("Break" code)
-		keyinfo->action = 0;
+	HalKeyboardEventQueue *new = kmalloc(sizeof(HalKeyboardEventQueue));
+	new->event = event;
+	new->previous = NULL;
+	if (HalKeyboardEventQueue_tail)
+	{
+		HalKeyboardEventQueue_tail->previous = new;
+		HalKeyboardEventQueue_tail = new;
+	}
 	else
-		// Pressed ("Make" code)
-		keyinfo->action = 1;
-
-	// Caps lock
-	if (SCANCODE(CAPSLOCK))
-		capslock = !capslock;
-
-	// Left shift
-	if (SCANCODE(SHIFT_LEFT))
-		shift_l = keyinfo->action;
-
-	// Right shift
-	if (SCANCODE(SHIFT_RIGHT))
-		shift_r = keyinfo->action;
-
-	// Alt
-	if (SCANCODE(ALT)) {
-		if ( escaped )
-			alt_r = keyinfo->action;
-		else
-			alt_l = keyinfo->action;
+	{
+		HalKeyboardEventQueue_head = new;
+		HalKeyboardEventQueue_tail = new;
 	}
-
-	// Control
-	if (SCANCODE(CONTROL)) {
-		if ( escaped )
-			ctrl_r = keyinfo->action;
-		else
-			ctrl_l = keyinfo->action;
-	}
-
-	if ( scancode == 0xe0 )
-		escaped = 1;
-	else
-		escaped = 0;
-
-	KEYINFO(shift_l);
-	KEYINFO(shift_r);
-	KEYINFO(alt_l);
-	KEYINFO(alt_r);
-	KEYINFO(ctrl_l);
-	KEYINFO(ctrl_r);
-
-	return keyinfo;
 }
 
-HalKeyInfo *HalKeyboardResolveScancode(HalKeyInfo *keyinfo)
+
+HalKeyboardEvent *HalKeyboardEventQueue_dequeue()
 {
-	keyinfo->key = keysym_us[keyinfo->scancode & 0x7F];
-	return keyinfo;
-}
-HalKeyInfo *HalKeyboardResolveScancode_shift(HalKeyInfo *keyinfo)
-{
-	keyinfo->key = keysym_us_shift[keyinfo->scancode & 0x7F];
-	return keyinfo;
+	if (!HalKeyboardEventQueue_head)
+		return NULL;
+	HalKeyboardEventQueue *head = HalKeyboardEventQueue_head;
+	HalKeyboardEvent *event = head->event;
+	HalKeyboardEventQueue_head = head->previous;
+	if (HalKeyboardEventQueue_tail == head)
+		HalKeyboardEventQueue_tail = NULL;
+
+	free(head);
+	return event;
 }
 
-HalKeyInfo *HalKeyboardReadLetter()
+HalKeyboardEvent *HalKeyboardGetEvent(int blocking)
 {
-	HalKeyInfo *keyinfo = kmalloc(sizeof(HalKeyInfo));
-	keyinfo = HalKeyboardRead();
-	if(keyinfo->shift_l || keyinfo->shift_r) {
-		keyinfo = HalKeyboardResolveScancode_shift(keyinfo);
-	} else {
-		keyinfo = HalKeyboardResolveScancode(keyinfo);
+	// Curtis' version uses Threading_yield(). We don't have Threading_yield() :(
+	if (blocking)
+		while (!HalKeyboardEventQueue_head)
+						__asm__("hlt");
+
+	return HalKeyboardEventQueue_dequeue();
+}
+
+HalKeyboardEvent *HalKeyboardReadLetter(int echo)
+{
+	HalKeyboardEvent *event = HalKeyboardGetEvent(1);
+	while (!event || !event->character || event->type == HalKeyboardEventType_up) {
+		free(event);
+		event = HalKeyboardGetEvent(1);
 	}
-	return keyinfo;
+	char character = event->character;
+	if (echo)
+		DisplayChar(character);
+	free(event);
+	return character;
 }
 
 void HalKeyboardInit()
 {
 	HalIrqHandler_Install(1, (void*)HalKeyboardHandler);
-	// The compiler is convinced that unsigned char[0x1000] is not an
-	// lvalue, so all these hacks are needed to compile.
-	buffer = buf;
-	origbuffer = buffer;
 }
 
-noreturn HalKeyboardTest()
-{
-	HalKeyInfo *keyinfo;
-	printf("Initiating keyboard test...\n");
-	while(1)
-	{
-		if ( HalKeyboardHasInput() ) {
-			keyinfo = HalKeyboardReadLetter();
-			if ( keyinfo->action == 0 ) {
-				printf("%c", keyinfo->key);
-			}
-		}
-	}
-}
