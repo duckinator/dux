b1f5ee90cba91b63ae88dc3759dacc57308fc989
Ellen Marie Dash <me@duckie.co>
2012-08-01 09:44:20 -0400

    Handle copying modules a bit better #2, because I forgot to pass '-a'

--- modules.mk
+++ modules.mk
@@ -2,7 +2,7 @@
 
 modules: metodo.exe test_module.exe uart.exe
 	@mkdir -p isofs/Modules
-	@cp src/modules/test_module.exe isofs/Modules/
+	./copymodules.sh
 
 test_module.exe: krnllib.lib libc.lib src/modules/test_module/test_module.o
 	@${LD} -o src/modules/test_module/test_module.exe ${LDFLAGS} -Ttext 0x200000 src/modules/test_module/test_module.o -Lsrc/lib/krnllib src/lib/krnllib/krnllib.lib -Lsrc/lib/libc src/lib/libc/libc.lib
