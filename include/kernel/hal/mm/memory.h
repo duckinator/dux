380960ed2dac9b73e6ae2d590d80824518ac9cef
Marie Markwell <me@marie.so>
2014-10-04 04:38:03 -0400

    More memory manager cleanup. o.o

--- include/kernel/hal/mm/memory.h
+++ include/kernel/hal/mm/memory.h
@@ -3,36 +3,6 @@
 
 #include <stdlib.h>
 
-typedef struct {
-	unsigned int present : 1;
-	unsigned int readwrite : 1;
-	unsigned int user : 1;
-	unsigned int writethrough : 1;
-	unsigned int cachedisable : 1;
-	unsigned int accessed : 1;
-	unsigned int dirty : 1;
-	unsigned int attributeindex : 1;
-	unsigned int global : 1;
-	unsigned int reserved : 3;
-	unsigned int address : 20;
-} PageTableEntry;
-
-typedef struct {
-	unsigned int present : 1;
-	unsigned int readwrite : 1;
-	unsigned int user : 1;
-	unsigned int writethrough : 1;
-	unsigned int cachedisable : 1;
-	unsigned int accessed : 1;
-	unsigned int available : 1;
-	unsigned int pagesize : 1;
-	unsigned int global : 1;
-	unsigned int reserved : 3;
-	unsigned int address : 20;
-} PageDirEntry;
-
-#define MALLOC_ALIGN 1
-
 #include <kernel/hal/mm/nicer_allocator.h>
 
 void HalMMInit();
